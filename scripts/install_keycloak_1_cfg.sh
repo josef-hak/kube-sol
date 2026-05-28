4d42571e22cb909fef94fa5d3ffd3049caafa929
Josef Hak <pepa.hak@gmail.com>
2026-05-28 19:33:41 +0200

    keycloak: add realm and user josef

--- scripts/install_keycloak_1_cfg.sh
+++ scripts/install_keycloak_1_cfg.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-helm upgrade --install keycloak-1-secrets \
+helm upgrade --install keycloak-cfg \
   oci://ghcr.io/k0rdent/catalog/charts/raw \
   --version 2.0.2 \
   -n keycloak \
   --create-namespace \
-  -f helm/keycloak-1-secrets.yaml
+  -f helm/keycloak-1-cfg.yaml
