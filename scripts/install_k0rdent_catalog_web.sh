52d2b241d00060da6207af492c959f59278df589
Josef Hak <pepa.hak@gmail.com>
2026-06-30 15:58:59 +0200

    Fix scripts/install_k0rdent_catalog_web.sh

--- scripts/install_k0rdent_catalog_web.sh
+++ scripts/install_k0rdent_catalog_web.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 helm upgrade --install k0rdent-catalog-web \
-  oci://ghcr.io/k0rdent/catalog/charts/k0rdent-catalog-web
+  oci://ghcr.io/k0rdent/catalog/charts/k0rdent-catalog-web \
   -n k0rdent-catalog-web \
   --create-namespace \
   -f helm/k0rdent-catalog-web.yaml
