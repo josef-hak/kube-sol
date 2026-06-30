ad7bd3e2b1fc71dd741ec9039086d43bbc1233e3
Josef Hak <pepa.hak@gmail.com>
2026-06-30 11:39:07 +0200

    install_k9s: add multi-platform support

--- scripts/install_k9s.sh
+++ scripts/install_k9s.sh
@@ -2,6 +2,28 @@
 
 # Install k9s to Ubuntu 24.04 LTS
 
-wget https://github.com/derailed/k9s/releases/latest/download/k9s_linux_amd64.deb && sudo apt install ./k9s_linux_amd64.deb && rm k9s_linux_amd64.deb
+arch="$(uname -m)"
+
+case "${arch}" in
+    x86_64|amd64)
+        arch="amd64"
+        ;;
+    aarch64|arm64)
+        arch="arm64"
+        ;;
+    armv7l)
+        arch="arm"
+        ;;
+    *)
+        echo "Unsupported architecture: ${arch}"
+        exit 1
+        ;;
+esac
+
+debfile="k9s_linux_${arch}.deb"
+
+wget https://github.com/derailed/k9s/releases/latest/download/${debfile}
+sudo apt install ./${debfile}
+rm ${debfile}
 
 echo "export TERM=xterm" >> ~/.bashrc
